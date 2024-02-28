f61fd0f6de06f42d628c8f564bead9a654d5f27a
Jihan Karunia Putri <jihankarunia09@gmail.com>
2024-02-28 15:04:40 +0700

    Percobaan Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+   });
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM = 2241720031 <br> Nama = Jihan Karunia Putri';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function($id){
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function($name=null){
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/{name?}', function($name='John'){
+    return 'Nama saya ' . $name;
 });
