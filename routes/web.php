04ae586fb8d56b93204934952a47ab43bbbade35
Jihan Karunia Putri <jihankarunia09@gmail.com>
2024-02-28 16:13:51 +0700

    Percobaan View

--- routes/web.php
+++ routes/web.php
@@ -71,3 +71,15 @@ use App\Http\Controllers\PhotoController;
 
 Route::resource('photos', PhotoController::class)->only(['index', 'show']);
 Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
+
+
+//VIEWS
+Route::get('/greeting', function(){
+    return view('hello', ['name' => 'Jihan']);
+});
+
+Route::get('/greeting', function(){
+    return view('blog.hello', ['name' => 'Jihan Karunia']);
+});
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
