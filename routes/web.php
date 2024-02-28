8dc061ee00683e521bea38b6d62aa4354a413069
Jihan Karunia Putri <jihankarunia09@gmail.com>
2024-02-28 15:56:29 +0700

    Percobaan Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -48,3 +53,21 @@ Route::get('/user/{name?}', function($name=null){
 Route::get('/user/{name?}', function($name='John'){
     return 'Nama saya ' . $name;
 });
+
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::get('/', [PageController::class,'index']);
+Route::get('/about', [PageController::class,'about']);
+Route::get('/articles', [PageController::class,'articles']);
+
+Route::get('/', [HomeController::class,'index']);
+Route::get('/about', [AboutController::class,'about']);
+Route::get('/articles', [ArticleController::class,'articles']);
+
+use App\Http\Controllers\PhotoController;
+//Route::resource('photos', PhotoController::class);
+
+
+Route::resource('photos', PhotoController::class)->only(['index', 'show']);
+Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
